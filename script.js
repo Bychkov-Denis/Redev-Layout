102a2bb0188b7b1251e2ee94ca46f6425e619c2c
Bychkov-Denis <byckovdenis3@gmail.com>
2026-02-04 01:16:01 +0300

    fix for review

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const swiper = new Swiper('.testimonials__slider', {
+  spaceBetween: 20,
+  speed: 500,
+  grabCursor: true,
+  freeMode: false,
+  centeredSlides: true,
+  centerInsufficientSlides: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesOffsetBefore: 20,
+    },
+  },
+});
+
 const header = document.querySelector('.header');
 const menuBtn = document.querySelector('.header__menu-btn');
 
